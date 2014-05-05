83b62d599339592ea6a50c9979d6ca5dce3ba8a2
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-05-04 22:58:48 -0700

    added other vendor prefixed audio contect contructors

--- theremin.js
+++ theremin.js
@@ -3,6 +3,9 @@
   var root = this;
   root.Theremin = {}
 
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  window.AudioContext = window.AudioContext || window.webkitAudioContext
+                                            || window.mozAudioContext
+                                            || window.oAudioContext
+                                            || window.msAudioContext;
 
 }).call(this);
