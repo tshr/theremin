98be02fd90ccdb480a1c583d44f7a2ab2e8a260a
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-05-04 23:26:16 -0700

    window-> root

--- theremin.js
+++ theremin.js
@@ -1,13 +1,13 @@
 (function() {
 
   var root = this;
-  root.Theremin = {}
+  root.Theremin = {};
 
-  window.AudioContext = window.AudioContext || window.webkitAudioContext
-                                            || window.mozAudioContext
-                                            || window.oAudioContext
-                                            || window.msAudioContext;
+  root.AudioContext = root.AudioContext || root.webkitAudioContext
+                                        || root.mozAudioContext
+                                        || root.oAudioContext
+                                        || root.msAudioContext;
 
-  if (!window.AudioContext) throw "The Web Audio API is not available in this environment"
+  if (!root.AudioContext) throw "The Web Audio API is not available in this environment";
 
 }).call(this);
