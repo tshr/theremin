2139b5c316496c64db11bf4030066ccc24cb3607
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-05-10 17:57:00 -0700

    added gain slider, some comments, reorg

--- theremin.js
+++ theremin.js
@@ -1,13 +1,16 @@
 (function() {
 
   var root = this;
-  root.Theremin = {};
 
+  // Set AudioContext to vendor specific versions
   root.AudioContext = root.AudioContext || root.webkitAudioContext
                                         || root.mozAudioContext
                                         || root.oAudioContext
                                         || root.msAudioContext;
 
   if (!root.AudioContext) throw "The Web Audio API is not available in this environment";
+
+  // Create Theremin global object
+  Theremin = root.Theremin = {};
 
 }).call(this);
