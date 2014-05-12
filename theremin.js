6964dd1fb116a1a7ab3496fbf1efb1dadec9075b
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-05-11 21:51:55 -0700

    root to this, added wouldnt it be nice

--- theremin.js
+++ theremin.js
@@ -1,24 +1,18 @@
 (function() {
 
-  var root = this;
-
   // Set AudioContext to vendor version
-  root.AudioContext = root.AudioContext || root.webkitAudioContext
-                                        || root.mozAudioContext
-                                        || root.oAudioContext
-                                        || root.msAudioContext;
+  this.AudioContext = this.AudioContext || this.webkitAudioContext
+                                        || this.mozAudioContext
+                                        || this.oAudioContext
+                                        || this.msAudioContext;
 
-  if (!root.AudioContext) throw "The Web Audio API is not available in this environment";
+  if (!this.AudioContext) throw "The Web Audio API is not available in this environment";
 
-  // Create Theremin root object
-  var _ = root.Theremin = {};
+  // Create Theremin object
+  var _ = this.Theremin = {};
 
   _.registerContext = function(context){
     return _.context = context;
-  };
-
-  _.getContext = function(){
-    return _.context;
   };
 
   _.playSound = function(buffer) {
