3d7ff063b369cae6f1f0e543e432e96727405c4b
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-05-17 15:47:18 -0700

    play/pause

--- theremin.js
+++ theremin.js
@@ -25,14 +25,16 @@
     return context;
   };
 
-  _.playBuffer = function(buffer) {
+  _.playBuffer = function(buffer, offset) {
 
     if (!context) throw "Theremin does not have a context, give Theremin an audio context by passing one to Theremin.setContext";
 
+    offset = typeof offset !== 'undefined' ? offset : 0;
     var source = context.createBufferSource();
     source.buffer = buffer;
+    source.loop = true;
     source.connect(context.destination);
-    source.start(0);
+    source.start(0, offset);
     return source;
   };
 
