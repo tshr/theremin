cda8dee961c99336c2bf4cf8d8d1b5089f5318a9
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-05-17 18:40:56 -0700

    added delay option

--- theremin.js
+++ theremin.js
@@ -34,6 +34,7 @@
     checkContext();
 
     options = typeof options !== 'undefined' ? options : {};
+    var delay = typeof options.delay !== 'undefined' ? options.delay : 0;
     var offset = typeof options.offset !== 'undefined' ? options.offset : 0;
     var loop = typeof options.loop !== 'undefined' ? options.loop : false;
     var source = context.createBufferSource();
@@ -41,7 +42,7 @@
     source.buffer = buffer;
     source.loop = loop;
     source.connect(context.destination);
-    source.start(0, offset);
+    source.start(delay, offset);
     return source;
   };
 
