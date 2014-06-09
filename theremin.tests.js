4f9d1c6072b3cb177b56cada43a08b230720d546
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-06-08 20:44:52 -0700

    added theremin.tests.js to hint and getContext / setContext tests

--- theremin.tests.js
+++ theremin.tests.js
@@ -1,7 +1,24 @@
 describe("Theremin object methods", function() {
   describe("getVersion", function() {
-    it("should return Theremin's version", function(){
+    it("returns Theremin's version", function(){
       expect(Theremin.getVersion()).toBe("0.0.1");
+    });
+  });
+
+  describe("getContext", function() {
+    it("gets Theremin's audio context or returns undefined if one is not set", function(){
+      context = new AudioContext();
+      expect(Theremin.getContext()).toBe(undefined);
+      Theremin.setContext(context);
+      expect(Theremin.getContext()).toBe(context);
+    });
+  });
+
+  describe("setContext", function() {
+    it("sets Theremin's audio context and returns the context", function(){
+      context = new AudioContext();
+      expect(Theremin.setContext(context)).toBe(context);
+      expect(Theremin.getContext()).toBe(context);
     });
   });
 });
