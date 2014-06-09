326697574f907468a8fe59e607844fa06841a5c2
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-06-08 20:54:06 -0700

    better descriptions and spaces

--- theremin.tests.js
+++ theremin.tests.js
@@ -1,24 +1,28 @@
-describe("Theremin object methods", function() {
-  describe("getVersion", function() {
-    it("returns Theremin's version", function(){
-      expect(Theremin.getVersion()).toBe("0.0.1");
-    });
-  });
+describe("Theremin", function(){
 
-  describe("getContext", function() {
-    it("gets Theremin's audio context or returns undefined if one is not set", function(){
-      context = new AudioContext();
-      expect(Theremin.getContext()).toBe(undefined);
-      Theremin.setContext(context);
-      expect(Theremin.getContext()).toBe(context);
-    });
-  });
+  describe("object methods", function(){
 
-  describe("setContext", function() {
-    it("sets Theremin's audio context and returns the context", function(){
-      context = new AudioContext();
-      expect(Theremin.setContext(context)).toBe(context);
-      expect(Theremin.getContext()).toBe(context);
+    describe("getVersion", function(){
+      it("returns version", function(){
+        expect(Theremin.getVersion()).toBe("0.0.1");
+      });
+    });
+
+    describe("getContext", function(){
+      it("gets audio context or returns undefined if one is not set", function(){
+        context = new AudioContext();
+        expect(Theremin.getContext()).toBe(undefined);
+        Theremin.setContext(context);
+        expect(Theremin.getContext()).toBe(context);
+      });
+    });
+
+    describe("setContext", function(){
+      it("sets audio context and returns it", function(){
+        context = new AudioContext();
+        expect(Theremin.setContext(context)).toBe(context);
+        expect(Theremin.getContext()).toBe(context);
+      });
     });
   });
 });
