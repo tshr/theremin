5b1bdaac87fff237a83bb63f6be5731376d71808
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-06-08 21:48:29 -0700

    player constructor tests

--- theremin.tests.js
+++ theremin.tests.js
@@ -10,7 +10,7 @@ describe("Theremin", function(){
 
     describe("getContext", function(){
       it("gets audio context or returns undefined if one is not set", function(){
-        context = new AudioContext();
+        var context = new AudioContext();
         expect(Theremin.getContext()).toBe(undefined);
         Theremin.setContext(context);
         expect(Theremin.getContext()).toBe(context);
@@ -19,9 +19,33 @@ describe("Theremin", function(){
 
     describe("setContext", function(){
       it("sets audio context and returns it", function(){
-        context = new AudioContext();
+        var context = new AudioContext();
         expect(Theremin.setContext(context)).toBe(context);
         expect(Theremin.getContext()).toBe(context);
+      });
+    });
+  });
+
+  describe("Player", function(){
+
+    describe("Player constructor", function(){
+
+      var player = new Theremin.Player();
+
+      it("new Theremin.Player() creates a new player object", function(){
+        expect(player.constructor).toBe(Theremin.Player);
+      });
+
+      it("new Theremin.Player() creates a new player object with accumulated_duration set to 0", function(){
+        expect(player.accumulated_duration).toBe(0);
+      });
+
+      it("new Theremin.Player() creates a new player object with loop set to true if true is passed as a parameter and false if nothing or false is passed", function(){
+        expect(player.loop).toBe(false);
+        player = new Theremin.Player(true);
+        expect(player.loop).toBe(true);
+        player = new Theremin.Player(false);
+        expect(player.loop).toBe(false);
       });
     });
   });
