363bc667555b853f4e5b490a7251d7974fecc546
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-06-10 23:11:42 -0700

    more play specs

--- theremin.tests.js
+++ theremin.tests.js
@@ -70,17 +70,7 @@ describe("Theremin", function(){
 
     describe("#play", function() {
 
-      afterEach(function() {
-        player.pause();
-      });
-
-      it("throws an exception if a buffer is not loaded", function() {
-        player = new Theremin.Player();
-        expect(function(){ player.play(); }).toThrow("No buffer loaded for this player");
-      });
-
-      it("if loop is set to true it sets the accumulated duration to the modulo of the accumulated duration over the buffer duration", function() {
-
+      beforeEach(function() {
         runs(function() {
           player.loadBuffer("http://upload.wikimedia.org/wikipedia/en/f/fd/Beach_Boys-wouldn_t_it_be_nice.ogg");
         });
@@ -88,6 +78,18 @@ describe("Theremin", function(){
         waitsFor(function() {
           return player.buffer;
         }, "Buffer failed to load", 5000);
+      });
+
+      afterEach(function() {
+        player.pause();
+      });
+
+      it("throws an exception if a buffer is not loaded", function() {
+        new_player = new Theremin.Player();
+        expect(function(){ new_player.play(); }).toThrow("No buffer loaded for this player");
+      });
+
+      it("if loop is true it sets the accumulated duration to the modulo of the accumulated duration over the buffer duration", function() {
 
         runs(function() {
           player.loop = true;
@@ -95,6 +97,22 @@ describe("Theremin", function(){
           player.play();
           expect(player.accumulated_duration).toBe(player.accumulated_duration % player.buffer.duration);
         });
+      });
+
+      it("if loop is false, the player is playing, and the accumulated duration is greater than buffer duration the player is reset", function() {
+
+        runs(function() {
+          player.loop = false;
+          player.accumulated_duration = 1000000;
+          player.play();
+          expect(player.accumulated_duration).toBe(0);
+        });
+      });
+
+      it("if the player is not playing it creates a new source", function() {
+        player.source = null
+        player.play();
+        expect(player.source.constructor).toBe(AudioBufferSourceNode);
       });
     });
   });
